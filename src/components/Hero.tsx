407ec7c70dc7f10c4522894c739a1d3a9247b095
njimogu wisdom <njimoguwisdom@gmail.com>
2025-06-13 10:06:57 +0100

    hero banner

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,15 +7,15 @@ const Hero = () => {
       {/* Overlay */}
       <div className="absolute inset-0 bg-black opacity-60 z-10"></div>
 
-      <section className="flex flex-col items-center justify-center z-20  text-center ">
+      <section className="flex flex-col items-center justify-center z-20 text-center ">
         <div className="w-3/4 max-md:w-full flex flex-col items-center justify-center z-20  text-center gap-8 max-md:px-3 ">
           <h1 className="font-extrabold text-5xl max-md:text-3xl w-3/4 bg-white bg-clip-text text-transparent drop-shadow-lg z-50">
             WECONETDATA TECHNOLOGIES
-            <p className="text-lg max-md:text-sm text-white/70">
+            <p className="text-lg max-md:text-sm text-blue-600/50 font-semibold mt-2">
               Powering Business Growth Through Smart Solutions
             </p>
           </h1>
-          <p className="max-md:text-md text-xl max-md:text-lg text-white/90 font-medium mb-2">
+          <p className="max-md:text-md text-xl max-md:text-lg text-white/70 font-medium mb-2 ">
             WeconetData Technologies is your trusted partner in smart business
             scaling. We build custom apps, websites, and mobile solutions that
             help your business scale faster and work smarter.
@@ -41,9 +41,9 @@ const Hero = () => {
         </div>
       </section>
       {/* Animated tech shapes */}
-      <div className="absolute top-10 left-10 w-40 h-32 bg-blue-400 opacity-20 rounded-full blur-2xl animate-pulse z-0 overflow-clip"></div>
-      <div className="absolute bottom-10 right-80 w-90 h-80 bg-primary opacity-20 rounded-full blur-lg animate-pulse z-0 overflow-clip"></div>
-      <div className="absolute top-10 left-30 w-30 h-30 bg-primary opacity-20 rounded-full blur-3xl animate-pulse z-0 overflow-clip"></div>
+      <div className="absolute top-10 left-10 w-40 h-32 bg-red-600 opacity-20 rounded-full blur-2xl animate-pulse z-0 overflow-clip"></div>
+      <div className="absolute bottom-10 right-80 w-90 h-90 bg-red-600 opacity-20 rounded-full blur-lg animate-pulse z-0 overflow-clip"></div>
+      <div className="absolute top-10 left-30 w-30 h-30 bg-red-600 opacity-20 rounded-full blur-3xl animate-pulse z-0 overflow-clip"></div>
     </div>
   );
 };
