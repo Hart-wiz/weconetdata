85751ed347572d3e82807b9de3810fe4e16155d5
njimogu wisdom <njimoguwisdom@gmail.com>
2025-06-13 16:07:28 +0100

    hero update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,19 +3,19 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="relative pt-30 h-screen w-full flex justify-center items-center bg-gradient-to-br from-primary via-blue-300 to-black overflow-hidden">
+    <div className="relative py-30  h-screen w-full flex justify-center items-center bg-gradient-to-br from-primary via-blue-300 to-black overflow-hidden">
       {/* Overlay */}
       <div className="absolute inset-0 bg-black opacity-60 z-10"></div>
 
       <section className="flex flex-col items-center justify-center z-20 text-center ">
-        <div className="w-3/4 max-md:w-full flex flex-col items-center justify-center z-20  text-center gap-8 max-md:px-3 ">
-          <h1 className="font-extrabold text-5xl max-md:text-3xl w-3/4 bg-white bg-clip-text text-transparent drop-shadow-lg z-50">
+        <div className="w-4/5 max-md:w-full flex flex-col items-center justify-center z-20  text-center gap-8 max-md:px-3 ">
+          <h1 className="font-extrabold text-6xl pt-30 max-md:text-3xl w-full bg-white bg-clip-text text-transparent drop-shadow-lg z-50">
             WECONETDATA TECHNOLOGIES
             <p className="text-lg max-md:text-sm text-blue-600/50 font-semibold mt-2">
               Powering Business Growth Through Smart Solutions
             </p>
           </h1>
-          <p className="max-md:text-md text-xl max-md:text-lg text-white/70 font-medium mb-2 ">
+          <p className="max-md:text-md text-lg max-md:text-lg text-white/70 font-medium mb-2 ">
             WeconetData Technologies is your trusted partner in smart business
             scaling. We build custom apps, websites, and mobile solutions that
             help your business scale faster and work smarter.
